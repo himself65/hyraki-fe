0d32f23d78743ee481001626dd782b3a235b2236
himself65 <himself65@outlook.com>
2019-08-12 17:40:13 +0800

    init src

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from '../reducers'
+
+const middlewares = [
+  thunkMiddleware
+]
+
+if (process.env.NODE_ENV === 'development' && process.env.TARO_ENV !== 'quickapp') {
+  middlewares.push(require('redux-logger').createLogger())
+}
+
+const enhancer = compose(
+  applyMiddleware(...middlewares)
+  // other store enhancers if any
+)
+
+export default function configStore () {
+  return createStore(rootReducer, enhancer)
+}
---
+++ src/views/Login/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const LoginView: React.FC = () => {
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default LoginView
