8b8923a2f6fa3a370caa0e5a94d902e28947097d
himself65 <himself65@outlook.com>
2019-08-16 16:26:33 +0800

    feat: add Sentry to watch errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/browser'
 import './index.less'
 import './style/theme.less'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+Sentry.init({ dsn: 'https://1fbf6fa0850d48748c77a03259fe3540@sentry.io/1532395' })
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
