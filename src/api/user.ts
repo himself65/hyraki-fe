3c74a439b6c294eadd96c5d2a149601e1c6f4192
himself65 <himself65@outlook.com>
2019-08-19 17:12:57 +0800

    style: fix lint

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,6 @@
 import axiosInstance from '.'
 import { store } from '../App'
-import { loginAction as loginAction } from '../store/action/user'
+import { loginAction } from '../store/action/user'
 
 export async function login (username: string, password: string) {
   return axiosInstance.post('/user/loginAction', {
