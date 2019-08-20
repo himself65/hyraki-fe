47b38a0240f3c275867e09c2b0f8445ab93db724
himself65 <himself65@outlook.com>
2019-08-20 23:10:14 +0800

    fix: only enable Sentry on production

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ import * as serviceWorker from './serviceWorker'
 // tip: config/webpack.dev.config.js 中定义了 DEBUG 全局变量
 if (DEBUG) {
   localStorage.debug = 'hyraki-fe:*'
+} else {
+  Sentry.init({ dsn: 'https://1fbf6fa0850d48748c77a03259fe3540@sentry.io/1532395' })
 }
-
-Sentry.init({ dsn: 'https://1fbf6fa0850d48748c77a03259fe3540@sentry.io/1532395' })
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
