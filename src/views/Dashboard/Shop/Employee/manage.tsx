039ee6d6b6a8817f2e84468430f8ab90060b8bc7
himself65 <himself65@outlook.com>
2019-09-05 22:14:56 +0800

    feat: update ManageContent

--- src/views/Dashboard/Shop/Employee/manage.tsx
+++ src/views/Dashboard/Shop/Employee/manage.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Row, Col, Button, List } from 'antd'
 
 export const ManageContent: React.FC = () => {
+  // todo: 获取数据
   return (
-    <div>
-      {/* todo */}
-    </div>
+    <Fragment>
+      <Row>
+        <Col>
+          <Button>
+            添加员工
+          </Button>
+        </Col>
+        <Col>
+          <Button>
+            职位管理
+          </Button>
+        </Col>
+        <Col>
+          <Button>
+            历史记录
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        {/* todo: 支持滚动加载 */}
+        <List/>
+      </Row>
+    </Fragment>
   )
 }
