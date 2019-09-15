848f420794f3da655cbeecc9abf4736304192f3b
himself65 <himself65@outlook.com>
2019-09-15 20:54:10 +0800

    style: simplify code

--- src/views/Dashboard/Dashboard/index.tsx
+++ src/views/Dashboard/Dashboard/index.tsx
@@ -11,18 +11,7 @@ import { getDashboardData } from '../../../api/dashboard'
 import { TodayData } from '../../../types/Dashboard'
 import { TodoData } from '../../../components/Dashboard/TodoBoard'
 import { Logger } from '../../../utils/debug'
-
 import './DashboardContent.less'
-
-// fixme: this is mock data, must remove these
-const visitData: { x: string, y: number }[] = []
-const beginDay = new Date().getTime()
-for (let i = 0; i < 20; i += 1) {
-  visitData.push({
-    x: moment(new Date(beginDay + 1000 * 60 * 60 * 24 * i)).format('YYYY-MM-DD'),
-    y: Math.floor(Math.random() * 100) + 10
-  })
-}
 
 export const Footer: React.FC = () => <HyFooter/>
 
