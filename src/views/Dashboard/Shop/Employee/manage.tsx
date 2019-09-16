a44dc30f1c1f11f29c0663dae16dbed1c21c0237
himself65 <himself65@outlook.com>
2019-09-16 21:22:49 +0800

    fix: simplify EmployeeManageContent code

--- src/views/Dashboard/Shop/Employee/manage.tsx
+++ src/views/Dashboard/Shop/Employee/manage.tsx
@@ -1,7 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
 import { Button, Col, Layout, Modal, Row } from 'antd'
 import { Subject } from 'rxjs'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { DefaultProps } from '../../../../types'
 import { getEmployeeList } from '../../../../api/employee'
 import EmployeeBriefList from '../../../../components/Employee/EmployeeBriefList'
@@ -13,6 +13,8 @@ const addEmployeeSubject = new Subject<boolean>()
 const ManageContent: React.FC<DefaultProps> = (props) => {
   const [employeeData, setEmployeeData] = useState([])
   const [showAddEmployeeModal, setShowAddEmployeeModal] = useState(false)
+  const EmployeeBrief: React.FC = useCallback(() => (
+    <EmployeeBriefList data={employeeData} existData={false}/>), [employeeData])
   useEffect(() => {
     const fetchData = async () => {
       await getEmployeeList().then(res => {
@@ -42,31 +44,29 @@ const ManageContent: React.FC<DefaultProps> = (props) => {
           </Button.Group>
         </Col>
       </Row>
-      <Layout>
-        <Modal
-          title='添加员工'
-          visible={showAddEmployeeModal}
-          onOk={() => {
-            addEmployeeSubject.next(true)
-          }}
-          onCancel={() => {
-            addEmployeeSubject.next(false)
-            setShowAddEmployeeModal(false)
-          }}
-        >
-          <AddEmployeeForm
-            subject={addEmployeeSubject}
-          />
-        </Modal>
-        <Switch>
-          <Route exact path='/dashboard/shop/employee' component={() => (
-            <Row>
-              <EmployeeBriefList data={employeeData} existData={false}/>
-            </Row>
-          )}/>
-          <Route path='/dashboard/shop/employee/position' component={PositionContent}/>
-        </Switch>
-      </Layout>
+      <Row>
+        <Layout>
+          <Switch>
+            <Route exact path='/dashboard/shop/employee/manage' component={EmployeeBrief}/>
+            <Route path='/dashboard/shop/employee/position' component={PositionContent}/>
+          </Switch>
+        </Layout>
+      </Row>
+      <Modal
+        title='添加员工'
+        visible={showAddEmployeeModal}
+        onOk={() => {
+          addEmployeeSubject.next(true)
+        }}
+        onCancel={() => {
+          addEmployeeSubject.next(false)
+          setShowAddEmployeeModal(false)
+        }}
+      >
+        <AddEmployeeForm
+          subject={addEmployeeSubject}
+        />
+      </Modal>
     </Fragment>
   )
 }
