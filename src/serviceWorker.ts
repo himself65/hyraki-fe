ce4a4905c2e87af45a4b12a5b8838df28043b227
himself65 <himself65@outlook.com>
2019-09-23 22:53:30 +0800

    fix: add error logs

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -102,6 +102,7 @@ function registerValidSW (swUrl: string, config?: Config) {
       }
     })
     .catch(error => {
+      console.error('swUrl:', swUrl)
       console.error('Error during service worker registration:', error)
     })
 }
