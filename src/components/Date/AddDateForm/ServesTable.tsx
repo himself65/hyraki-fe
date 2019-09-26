97a30db21da4aaf835c5996effda1c439e8a8f27
himself65 <himself65@outlook.com>
2019-09-26 18:26:45 +0800

    feat: update SearchSelect on ServesTable

--- src/components/Date/AddDateForm/ServesTable.tsx
+++ src/components/Date/AddDateForm/ServesTable.tsx
@@ -8,23 +8,26 @@ interface Props extends FormComponentProps {
   serves: Serve[]
 }
 
-const filterNames = (serves: Serve[]) => serves.map(v => v.name)
+const filterNames = (serves: Serve[]) => serves.map(v => v.name).sort()
 
 const ServesTable: React.FC<Props> = (props) => {
   const form = props.form
   const [orderedServes, setOrderedServes] = useState<Serve[]>([])
   const [options, setOptions] = useState<string[]>([])
   const serveNames = useMemo(() => filterNames(props.serves), [props.serves])
-  const handleAddServeItem = useCallback((item: string) => {
-
-  }, [])
+  // todo: 添加自动 Search 的方法
   return (
     <div>
       <Select
         showSearch
         placeholder='搜索需要输入的服务'
-        onSearch={handleAddServeItem}
-      >
+        onChange={value => {
+          // todo: 添加到 Table 中
+        }}
+        filterOption={(input, option) =>
+          (option.props.children! as string)
+            .toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }>
         {options.map(v => (<div/>))}
       </Select>
       <Table
