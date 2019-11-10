b44c202154fc24077aa64d31273cf11c4a1c4ad5
himself65 <himself65@outlook.com>
2019-11-10 15:47:23 +0800

    fix: use Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Redirect, Route, Switch } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 import LoadingView from './views/Loading'
 import ErrorView from './views/Error'
+import { Provider } from 'mobx-react'
+import { store } from './store'
 
 const asyncComponentFactory = (resolve: () => Promise<React.ComponentType<any> | { default: React.ComponentType<any> }>) =>
   asyncComponent({
@@ -15,22 +17,24 @@ const asyncComponentFactory = (resolve: () => Promise<React.ComponentType<any> |
   })
 
 const DashboardView = asyncComponentFactory(() => import('./views/Dashboard'))
-const LoginView = asyncComponentFactory(() => import('./views/Start'))
+const StartView = asyncComponentFactory(() => import('./views/Start'))
 const RegisterView = asyncComponentFactory(() => import('./views/Register'))
 
 const App: React.FC = () => {
   return (
     <AsyncComponentProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={LoginView}/>
-          <Route path='/login' component={LoginView}/>
-          <Route path='/dashboard' component={DashboardView}/>
-          <Route path='/register' component={RegisterView}/>
-          <Route path='/error' component={ErrorView}/>
-          <Redirect from='/*' to='/error'/>
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={StartView}/>
+            <Route path='/login' component={StartView}/>
+            <Route path='/dashboard' component={DashboardView}/>
+            <Route path='/register' component={RegisterView}/>
+            <Route path='/error' component={ErrorView}/>
+            <Redirect from='/*' to='/error'/>
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </AsyncComponentProvider>
   )
 }
