6e8c25f757d88a3cc975cdefde3103d409225eff
himself65 <himself65@outlook.com>
2019-11-10 16:28:08 +0800

    refactor: StartView

--- src/views/Start/index.tsx
+++ src/views/Start/index.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Col, Icon, Layout, Menu, Row } from 'antd'
-import { HyContent, HyFooter, HyHeader, HyLayout, HySidebar } from '../../components/Layout'
-import LoginBoard from '../../components/LoginBoard'
+import React, { useEffect } from 'react'
+import { Card, ICardTokens } from '@uifabric/react-cards'
+import { HyLayout } from '../../components/Layout'
 import './index.less'
 import { DefaultProps } from '../../../types'
 import { observer } from 'mobx-react'
 import { store } from '../../store'
+import LoginBoard from '../../components/LoginBoard'
 
 interface StartViewProps extends DefaultProps {
   loginMessage?: string
   logout: boolean
+}
+
+const cardTokens: ICardTokens = {
+  minWidth: 500
 }
 
 const StartView: React.FC<StartViewProps> = (props) => {
@@ -18,69 +22,17 @@ const StartView: React.FC<StartViewProps> = (props) => {
       props.history.push('/dashboard')
     }
   }, [])
-  const [loginBoard, setLoginBoard] = useState(false)
   return (
     <HyLayout className='login-view'>
-      <HySidebar
-        breakpoint='lg'
-        collapsedWidth='0'
-        width='320'
-      >
-        <HyHeader>
-          <Menu className='hy-sider-nav'
-            style={{ background: 'transparent' }}
-            mode='horizontal'
-            defaultOpenKeys={['main']}
-          >
-            <Menu.Item className='hy-sider-nav-item'
-              key='main'
-            >
-              主页
-            </Menu.Item>
-            <Menu.Item className='hy-sider-nav-item'
-              key='about'
-            >
-              关于
-            </Menu.Item>
-          </Menu>
-        </HyHeader>
-        <div className='logo'/>
-        <div className='hy-menu'>
-          {
-            loginBoard
-              ? <LoginBoard/>
-              : (
-                <Row
-                  className='login'
-                  type='flex'
-                  justify='center'
-                  align='middle'
-                >
-                  <Col>
-                    <Button onClick={() => setLoginBoard(true)}
-                      size='large'
-                      type='link'
-                    >
-                      <Icon type='user'/>
-                      登录
-                    </Button>
-                  </Col>
-                  <Col>
-                    <Button size='large'>
-                      注册
-                    </Button>
-                  </Col>
-                </Row>
-              )
-          }
-        </div>
-      </HySidebar>
-      <Layout>
-        <HyContent>
-          Todo
-        </HyContent>
-        <HyFooter/>
-      </Layout>
+      <div>
+        <Card
+          tokens={cardTokens}
+          className={'login-card'}
+        >
+          <h1 className='title'>登录</h1>
+          <LoginBoard/>
+        </Card>
+      </div>
     </HyLayout>
   )
 }
