09cca7f3728bfe1ecf0d97e5da03941cbaf33c61
himself65 <himself65@outlook.com>
2019-12-07 19:44:10 +0800

    fix: add a comment

--- src/api/reserve/index.ts
+++ src/api/reserve/index.ts
@@ -2,6 +2,7 @@ import axiosInstance from '../'
 import { Reserve } from '~type/Reserve'
 import { axiosHandle } from '~util/helpers'
 
+// fixme: 后端尚未支持
 export function postAddReserve (data: Reserve) {
   return axiosInstance.post('/reserves', {
     ...data
