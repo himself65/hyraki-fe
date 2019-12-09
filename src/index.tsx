597bec86c1579066a8c6804f6e9a82675764c914
himself65 <himself65@outlook.com>
2019-12-09 19:40:28 +0800

    feat: loadTheme

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Sentry from '@sentry/browser'
 import { initializeIcons } from '@uifabric/icons'
+import { loadTheme } from 'office-ui-fabric-react/lib/Styling'
 import 'ant-design-pro/lib/Charts/style/index.less'
 import 'office-ui-fabric-react/dist/sass/Fabric.scss'
 import './index.less'
 import './style/theme.less'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+// fixme: 需要敲定配色
+loadTheme({
+  palette: {
+    themePrimary: '#FF6400',
+    themeLighterAlt: '#eff6fc',
+    themeLighter: '#deecf9',
+    themeLight: '#c7e0f4',
+    themeTertiary: '#71afe5',
+    themeSecondary: '#FFDC3C',
+    themeDarkAlt: '#106ebe',
+    themeDark: '#005a9e',
+    themeDarker: '#004578',
+    neutralLighterAlt: '#f8f8f8',
+    neutralLighter: '#f4f4f4',
+    neutralLight: '#eaeaea',
+    neutralQuaternaryAlt: '#dadada',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c8c8',
+    neutralTertiary: '#c2c2c2',
+    neutralSecondary: '#858585',
+    neutralPrimaryAlt: '#4b4b4b',
+    neutralPrimary: '#333333',
+    neutralDark: '#272727',
+    black: '#1d1d1d',
+    white: '#ffffff'
+  }
+})
 
 // tip: config/webpack.dev.config.js 中定义了 DEBUG 全局变量
 if (DEBUG) {
