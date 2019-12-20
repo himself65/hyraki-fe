4082dc2915f5757277390b9c580e97e6befc70ad
himself65 <himself65@outlook.com>
2019-12-20 16:24:57 +0800

    fix: lint

--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,19 @@ module.exports = ctx => ({
   plugins: [
     require('precss'),
     require('cssnano'),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('autoprefixer')({
       remove: false
     }),
     require('postcss-flexbugs-fixes'),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
     }),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('postcss-normalize')()
   ]
 })
